92bcbf852c028af5160ada5ff6c720624d1cdba0
Mateja Abrams <mateja81@gmail.com>
2015-04-30 00:19:02 -0700

    express server made, secret page, 404 page

---
+++ app.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+app.get('/secret', function(req, res) {
+  res.status(200).sendFile(__dirname + '/public/secret.html');
+});
+app.get('/*', function(req,res) {
+  res.status(404).send('404 Page Not Here!');
+});
+app.listen(5000, function() {
+  console.log('server operational!');
+});
